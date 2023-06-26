6736e2d36d94094b5d5593e21390b50983bafbe5
aykae <raiaboudi@yahoo.com>
2023-06-25 20:22:25 -0400

    began basic sentence generator

--- generator.py
+++ generator.py
@@ -1,7 +1,10 @@
+import random
+
 class Grammar:
 
     def __init__(self):
         self.grammar = {}
+        self.initGrammar()
     
     #Grammar is defined as a dictionary that maps a string to a string.
     #Grammar based on UCLA Prof. Hayes Introductory Linguistics Text, (Figure 116) Phrase Structure Rules V1
@@ -25,4 +28,35 @@ class Grammar:
         self.grammar["V"] = ("speaks", "jumps", "learns")
         self.grammar["Adj"] = ("tall", "loud", "big", "expensive")
 
+    def generateSentence(self):
+        sentence = ""
+        state = "S" 
+        
+        return self.recurseGrammar(sentence, state)
+   
+    def recurseGrammar(self, sentence, state):  
+        print(sentence) 
+        if state:
+            exprVal = self.grammar[state]
 
+            if isinstance(exprVal, tuple): #we have reached a terminal symbol
+                state = None
+                sentence += random.choice(exprVal)
+                sentence += " "
+                return sentence
+            elif isinstance(exprVal, list): #recurse to the next non-terminal symbol
+                expr = random.choice(exprVal)
+                for sym in expr.split(" "):
+                    if sym[0] == "(": #Remove optional parentheses
+                        sym = sym[1:-1]
+                    state = sym
+                    sentence += self.recurseGrammar(sentence, state)
+        return ""
+
+if __name__ == '__main__':
+    g = Grammar()
+    g.initGrammar()
+    
+    print(g.grammar)
+    print("Sentence: ")
+    print(g.generateSentence())
---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+from generator import Grammar
+
+class TestGrammarParse(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(2, 2, "Expected: True")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
