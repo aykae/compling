8b41e76b45186bdc94e736a05871cc6e0fe53505
aykae <abdrai19@gmail.com>
2023-07-11 06:57:54 -0400

    new app, grammarcheck

---
+++ grammarcheck.py
@@ -0,0 +1,5 @@
+# MAIN FEATURE
+#Takes in an English sentence, and determines whether the sentence is grammatical or not.
+    #Future Features
+        # - determine which part of sentence is ungrammatical
+        # - suggest a fix to make the sentence grammatical
